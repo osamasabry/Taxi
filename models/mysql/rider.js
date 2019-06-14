8f200af4edf729a5db973b4fc78c80e7da63867b
Mahmoud Kamel <mahmoudkamel@outlook.com>
2019-06-14 23:14:38 +0200

    fix

--- models/mysql/rider.js
+++ models/mysql/rider.js
@@ -70,5 +70,12 @@ module.exports = {
     getContactInformation: async function (riderId) {
         let [result,ignored] = await sql.query("SELECT travel.id as id, driver.last_name as driverName,driver.mobile_number AS driverNumber,rider.last_name as riderName,rider.mobile_number as riderNumber FROM travel INNER JOIN rider ON rider.id = travel.rider_id INNER JOIN driver ON driver.id = travel.driver_id WHERE rider_id = ? ORDER BY travel.id DESC LIMIT 1", [riderId]);
         return result[0];
-    }
+    },
+
+     getStatus: async function (riderId) {
+        console.log(riderId)
+        let [result, ignored] = await sql.query("SELECT status FROM rider WHERE id = ?", [riderId]);
+        // console.log(result);
+        return result[0].status;
+    },
 };
